9e449f3165c9c05113e0ae3b04c3b7d2be1e5bc6
salman-bhai <sbs.191197@gmail.com>
2017-03-27 15:54:40 +0530

    Repo Android App updated

--- Android App/app/src/main/java/com/hrily/notesnearby/OnAzimuthChangedListener.java
+++
@@ -1,8 +0,0 @@
-package com.hrily.notesnearby;
-
-/**
- * Created by krzysztofjackowski on 24/09/15.
- */
-public interface OnAzimuthChangedListener {
-    void onAzimuthChanged(float azimuthFrom, float azimuthTo);
-}
